f57127d62ec4b950f2dc03232feae8ce777465a8
Christopher Soriano <chrissoriano1313@gmail.com>
2022-06-03 16:38:02 -0700

    Added Drink images and content in DrinkMenu

--- src/components/Drinkmenu/Drinkmenu.js
+++ src/components/Drinkmenu/Drinkmenu.js
@@ -1,6 +1,7 @@
 import "./DrinkMenu.css";
 
-import wine from "../../img/8bit-wine.gif";
+import magic from "../../img/8bit-magic-potion.gif";
+import galaxy from "../../img/8bit-galaxy-potion.gif";
 import Subheading from "../Subheading/Subheading";
 import NavbarAlt from "../NavbarAlt/NavbarAlt";
 
@@ -11,8 +12,7 @@ export default function DrinkMenu() {
       <section className="app__menu-section">
         <div className="app__menu">
           <div className="app__drink-image image-left">
-            <img src={wine} alt="8bit-wine" />
-            <img src={wine} alt="8bit-wine" />
+            <img src={magic} alt="8bit-magic-potion" />
           </div>
           <div className="app__menu-center">
             <Subheading title="Classy" subtitleDark="From the Bar" />
@@ -57,49 +57,49 @@ export default function DrinkMenu() {
                 </p>
               </div>
               <div className="app__menu-item">
-                <h3>The Blue Devil</h3>
+                <h3>The Paralyzer</h3>
                 <p>
-                  Humboldt Hemp Vodka, blueberry honey, lime, house-made
-                  lavender biters
+                  New Amsterdam Gin, Yellow Chartreuse, Giffard's Passion Fruit
+                  Liqueur, lemon up
                 </p>
               </div>
               <div className="app__menu-item">
-                <h3>King Koopa Killer</h3>
+                <h3>Bubble Bobble Bubble</h3>
                 <p>
-                  St, George Green Chile Vodka, lemon, agave, elderflower
-                  liqueur, jalapeno rocks
+                  Louisiana Bayou White Rum, Aperol, lemon, grilled pineapple,
+                  pink peppercorn tincture. Tail.
                 </p>
               </div>
               <div className="app__menu-item">
-                <h3>Do a Barrel Roll</h3>
+                <h3>Ekans Loi</h3>
                 <p>
-                  Cutwater Barrel-Rested Gin, Lillet Blanc, Guildhall Amaro
-                  Municipio, Herbsaint rinse
+                  Marti 3 year rum, Gosling's black seal rum, green chili vodka,
+                  pineapple juice, citrus blend, honey, angostura bitters,
+                  crushed ice.
                 </p>
               </div>
               <div className="app__menu-item">
-                <h3>1984</h3>
-                <p>Laird's Applejack Brandy, Foro Amaro, lemon, salted honey</p>
-              </div>
-              <div className="app__menu-item">
-                <h3>Cherry Pac-Blast</h3>
+                <h3>Duck Hunter</h3>
                 <p>
-                  Four Roses Bourbon, Demerara sugar, orange, compound bitters,
-                  maraschino cherry
+                  Astral Pacific Gin, Green Chatreuse, Luxardo Marashino
+                  Liqueur, lemon. Up.
                 </p>
               </div>
               <div className="app__menu-item">
-                <h3>Phoenix Down</h3>
+                <h3>Little Icarus</h3>
                 <p>
-                  Old Overholt Rye, Guildhall Palazzo Vermouth, Benedictine
-                  D.O.M, Foro Amaro, aromatized Laphroaig
+                  Four Roses Bourbon, luxardo Maraschino, lime,
+                  blackberry-balsamic shurb, aquadfaba. Foamy
                 </p>
+              </div>
+              <div className="app__menu-item">
+                <h3>Metroid Primer</h3>
+                <p>Machetazo mezcal, lime juice, grenadine. Up.</p>
               </div>
             </div>
           </div>
           <div className="app__drink-image image-right">
-            <img src={wine} alt="8bit-wine" />
-            <img src={wine} alt="8bit-wine " className="second-image" />
+            <img src={galaxy} alt="8bit-galaxy-potion" />
           </div>
         </div>
       </section>
--- src/components/Foodmenu/Foodmenu.js
+++ src/components/Foodmenu/Foodmenu.js
@@ -1,6 +1,6 @@
 import "./FoodMenu.css";
 
-import fried from "../../img/8bit-fried-chicken.gif"
+import fried from "../../img/8bit-fried-chicken.gif";
 import chicken from "../../img/8bit-link-chicken.gif";
 import Subheading from "../Subheading/Subheading";
 import NavbarAlt from "../NavbarAlt/NavbarAlt";
@@ -58,43 +58,41 @@ export default function FoodMenu() {
                 </p>
               </div>
               <div className="app__menu-item">
-                <h3>8-Bit Bites Bites</h3>
+                <h3>Poke Monster Burger</h3>
                 <p>
-                  Three 8 oz brisket burger sliders stuffed with smoked cheddar,
-                  topped with caramelized onions, pickles, special sauce,
-                  brioche bun
+                  1 lb brisket burger stuffed with smoked cheddar, topped with
+                  caramelized onions, pickles, special sauce, brioche bun
+                </p>
+                <p>Choose between 2 inspired Pokemons: Pikachu and Eevee</p>
+              </div>
+              <div className="app__menu-item">
+                <h3>Yoshi Egg Omelet</h3>
+                <p>Japanese rice omelet, served with ketchup drizzled on top</p>
+              </div>
+              <div className="app__menu-item">
+                <h3>Cheep-Cheep Sashimi</h3>
+                <p>Raw salmon, pickled ginger, ponzu sauce</p>
+              </div>
+              <div className="app__menu-item">
+                <h3>Little Mac & Cheese</h3>
+                <p>
+                  Three cheese blend, herbed bread crumbs, chili oil (Add bacon
+                  or caramelized onion)
                 </p>
               </div>
               <div className="app__menu-item">
-                <h3>Pac-Man Pac-Chips</h3>
-                <p>Add chilled habanero or pimento cheese dip</p>
-                <p>Add buffalo or ranch seasoning dust</p>
-              </div>
-              <div className="app__menu-item">
-                <h3>Kakariko Chicken Strips</h3>
+                <h3>Blanka Veggie Wrap</h3>
                 <p>
-                  Tortilla chip crust, dry buffalo seasoning , house ranch,
-                  crudite
+                  Mixed greens, portobello mushroom, tomato, cucumber, red
+                  onion, feta, piquillo pepper hummus, tahini, spinach tortilla
                 </p>
+                <p>Add grilled chicken</p>
               </div>
               <div className="app__menu-item">
-                <h3>Maxim Tomato Soup</h3>
+                <h3>Princess Peach's Cheesecake</h3>
                 <p>
-                  Served with slice of cheesy garlic bread, in cup or bowl sizes
-                </p>
-              </div>
-              <div className="app__menu-item">
-                <h3>Tour De Tostarenan</h3>
-                <p>
-                  Three tacos with chorizo, bacon, fig & mascarpone spread,
-                  mango-habanero chutney
-                </p>
-              </div>
-              <div className="app__menu-item">
-                <h3>Super Mario Pizza</h3>
-                <p>
-                  Italian sausage, seasoned ground beef, pepperoni, ham, salami
-                  & bacon
+                  Classic cream cheese cheesecake with amaretto and a chocolate
+                  almond crust
                 </p>
               </div>
             </div>
