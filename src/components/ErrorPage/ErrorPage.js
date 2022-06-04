40d60b02eab58c8db5cab3c12cf37bd1c3231c6c
Christopher Soriano <chrissoriano1313@gmail.com>
2022-06-03 20:45:54 -0700

    Added ErrorPage Content

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,12 +1,22 @@
-import "./ErrorPage.css"
+import "./ErrorPage.css";
 
 import NavbarAlt from "../NavbarAlt/NavbarAlt";
 
+import running from "../../img/8bit-mario-running.gif";
+
 export default function ErrorPage() {
-  return (<>
-    <NavbarAlt />
-    <div className="app__error-page">
-      <h1>Error! This page does not exist!</h1>
-    </div>
-  </>)
+  return (
+    <>
+      <NavbarAlt />
+      <div className="app__error-page">
+        <div className="app__error-section">
+          <h1>Error!</h1>
+          <h2>It seems you have lost your way!</h2>
+          <div className="app__error-image">
+            <img src={running} alt="8bit-mario-running" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
